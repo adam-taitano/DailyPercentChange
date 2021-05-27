694fe1b8b8145e51602ce13a78ce7f90d54a6167
adam-taitano <79883367+adam-taitano@users.noreply.github.com>
2021-05-27 13:11:49 -0700

    Responsive
    attempt

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var userId = url.split("#")[1].split("=")[2].split("&")[0];
 console.log(access_token);
 console.log(userId);
 
+document.getElementById("username").innerHTML = userId
+
 var xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://api.fitbit.com/1/user/' + userId + '/activities/steps/date/today/1w.json');
 xhr.setRequestHeader("Authorization", 'Bearer ' + access_token);
