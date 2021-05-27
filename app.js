b59a2d77d8d3a67b5b6705f9b532da9ef4a5ee2d
adam-taitano <79883367+adam-taitano@users.noreply.github.com>
2021-05-27 13:13:45 -0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var userId = url.split("#")[1].split("=")[2].split("&")[0];
 console.log(access_token);
 console.log(userId);
 
-document.getElementById("username").innerHTML = userId
+document.getElementById("username").innerHTML = userId;
 
 var xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://api.fitbit.com/1/user/' + userId + '/activities/steps/date/today/1w.json');
