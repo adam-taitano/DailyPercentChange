86fa85402176dc24f3433c4fa0d39beb77b770dc
adam-taitano <79883367+adam-taitano@users.noreply.github.com>
2021-05-28 14:13:28 -0700

    Update app.js

--- nodetest1/app.js
+++ nodetest1/app.js
@@ -4,6 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// New Code to connect MongoDB
+var monk = require('monk');
+var db = monk('localhost:27017/nodetest1');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
